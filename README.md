a8dad4a53be0747f56521c0026d3d743525de765
AZysk0 <arko.zystem@gmail.com>
2024-07-20 18:35:28 +0300

    deps file

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # winstars-ai-test-2024
-
-# winstars-ai-test-2024
--- inference.py
+++ inference.py
@@ -12,7 +12,7 @@ import cv2
 
 # tensorflow
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow.python import keras
 from keras.layers import (Conv2D, Input, MaxPooling2D, 
                                      Dropout, concatenate, UpSampling2D, BatchNormalization, Conv2DTranspose)
 
@@ -52,8 +52,115 @@ def image_from_path(img_dir, img_name):
     
     return np.expand_dims(image, axis=0)
 
+# ======== Unet
+def create_conv2d_block(input_tensor, num_filters, kernel_size=3, batchnorm=True):
+    """Function to add 2 convolutional layers with the parameters passed to it"""
+    
+    # 1st layer
+    x = Conv2D(filters = num_filters, kernel_size = (kernel_size, kernel_size),\
+              kernel_initializer = 'he_normal', padding = 'same')(input_tensor)
+    
+    if batchnorm:
+        x = BatchNormalization()(x)
+        
+    x = keras.layers.Activation('relu')(x)
+    
+    # 2nd layer
+    x = Conv2D(filters = num_filters, kernel_size = (kernel_size, kernel_size),\
+              kernel_initializer = 'he_normal', padding = 'same')(input_tensor)
+    
+    if batchnorm:
+        x = BatchNormalization()(x)
+    
+    x = keras.layers.Activation('relu')(x)
+    
+    return x
 
+def create_unet(input_shape, num_filters=16, dropout=0.1, batchnorm=True):
+    """
+    Function to define the UNET Model
+    input_shape: (height, width, 3)
+    """
+    
+    assert input_shape[-1] == 3  # image must have 3 channels
+    
+    # input 'layer'
+    #input_img = Input((*input_shape, 3), name='img')
+    input_img = Input(input_shape, name='img')
+    
+    # downsampling (encoder)
+    c1 = create_conv2d_block(input_img, num_filters * 1, kernel_size=3, batchnorm=batchnorm)
+    p1 = MaxPooling2D((2, 2))(c1)
+    p1 = Dropout(dropout)(p1)
+    
+    c2 = create_conv2d_block(p1, num_filters * 2, kernel_size=3, batchnorm=batchnorm)
+    p2 = MaxPooling2D((2, 2))(c2)
+    p2 = Dropout(dropout)(p2)
+    
+    c3 = create_conv2d_block(p2, num_filters * 4, kernel_size=3, batchnorm=batchnorm)
+    p3 = MaxPooling2D((2, 2))(c3)
+    p3 = Dropout(dropout)(p3)
+    
+    c4 = create_conv2d_block(p3, num_filters * 8, kernel_size=3, batchnorm=batchnorm)
+    p4 = MaxPooling2D((2, 2))(c4)
+    p4 = Dropout(dropout)(p4)
+    
+    # bottleneck
+    c5 = create_conv2d_block(p4, num_filters * 16, kernel_size=3, batchnorm=batchnorm)
+    
+    # upsampling (decoder)
+    u6 = Conv2DTranspose(num_filters * 8, (3, 3), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    u6 = Dropout(dropout)(u6)
+    c6 = create_conv2d_block(u6, num_filters * 8, kernel_size=3, batchnorm=batchnorm)
+    
+    u7 = Conv2DTranspose(num_filters * 4, (3, 3), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    u7 = Dropout(dropout)(u7)
+    c7 = create_conv2d_block(u7, num_filters * 4, kernel_size=3, batchnorm=batchnorm)
+    
+    u8 = Conv2DTranspose(num_filters * 2, (3, 3), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    u8 = Dropout(dropout)(u8)
+    c8 = create_conv2d_block(u8, num_filters * 2, kernel_size=3, batchnorm=batchnorm)
+    
+    u9 = Conv2DTranspose(num_filters * 1, (3, 3), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1])
+    u9 = Dropout(dropout)(u9)
+    c9 = create_conv2d_block(u9, num_filters * 1, kernel_size=3, batchnorm=batchnorm)
+    
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+    model = Model(inputs=[input_img], outputs=[outputs])
+    
+    return model
 
+def make_prediction(model, image):
+    threshold = 0.5
+    
+    # get probability for each pixel (-> np.ndarray)
+    prob_mask = model.predict(image).squeeze(0)
+    
+    segmentation_mask = np.zeros(prob_mask.shape)
+    # Convert probabilities to binary segmentation mask
+    segmentation_mask = (prob_mask > threshold).astype(np.uint8)
+    return segmentation_mask
 
+input_shape = (*NEW_SHAPE, 3)
+unet_weights_path = 'model-Unet.weights.h5'
 
+unet_model = create_unet(input_shape)
+unet_model.load_weights(unet_weights_path)
 
+test_images_folder_path = 'data/test'
+test_images_filenames =  [os.path.basename(filename) for filename in glob.glob(test_images_folder_path + '/*')]
+
+print(len(test_images_filenames), test_images_filenames[:10])
+
+some_image = image_from_path(test_images_folder_path, test_images_filenames[1])
+some_image_mask = make_prediction(unet_model, some_image)
+
+fig, ax = plt.subplots(ncols=2)
+
+ax[0].imshow(some_image.squeeze(0))
+ax[1].imshow(some_image_mask, cmap='gray')
+plt.show()
